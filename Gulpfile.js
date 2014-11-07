720821c7ec4899fdd605bcd8fc26b82b2364c6b7
Tomohiko Himura <eiel.hal@gmail.com>
2014-11-07 21:23:46 +0900

    Setup BrowserSync

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 var assemble = require('gulp-assemble');
 var htmlmin = require('gulp-htmlmin');
+var browserSync = require('browser-sync');
+var runSequence = require('gulp-run-sequence');
+var watch = require('gulp-watch');
+
+gulp.task('build', function(cb) {
+  runSequence('build-clean', ['build-scripts', 'build-styles'], 'build-html', cb);
+});
 
 var options = {
   data: 'data/*.json',
@@ -8,11 +15,35 @@ var options = {
   layoutdir: 'templates/layouts/'
 };
 
+var dist = gulp.dest('dist');
+
+gulp.task('watch', function () {
+  watch('dist/**/*.html', function(files) {
+    return files
+      .pipe(browserSync.reload({stream:true}));
+  });
+  watch('templates/pages/*.hbs', function(files) {
+    return files
+      .pipe(assemble(options))
+      .pipe(dist);
+  });
+});
+
 gulp.task('assemble', function () {
   gulp.src('templates/pages/*.hbs')
     .pipe(assemble(options))
     .pipe(htmlmin())
-    .pipe(gulp.dest('_gh_pages/'));
+    .pipe(dist);
 });
 
-gulp.task('default', ['assemble']);
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: "dist/"
+    }
+  });
+});
+
+gulp.task('default', function() {
+  runSequence("assemble",['watch','browser-sync']);
+});
